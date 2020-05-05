4b8ec0c8af38256bc1239598edcd48ecaf3d3968
theKoladeAkande <akandekolade@gmail.com>
2020-05-05 17:02:58 +0100

    feat: Handles connection to the database
    Handles connection to the database, handles error and
    ensures cursors and connections stays closed when not in use.

---
+++ connection_manger.py
@@ -0,0 +1,43 @@
+import psycopg2
+import contextlib
+
+def psycopg2_error_handler(fn):
+    """Function decorator for handling psycopg2 errors
+       :fn: a function that executes postgresql queries
+    """
+    def inner(*args, **kwargs):
+        try:
+            fn(*args, **kwargs)
+        except psycopg2.Error as e:
+            print(e)
+            raise
+    return inner
+
+@contextlib.contextmanager
+def dbconnection_manager(dbname, user, password, host, port):
+    """
+    Context manager to handle database connection
+    param dbname: name of database
+    """
+    connection_state = False
+    try:
+        conn =  psycopg2.connect(f"dbname={dbname} user={user} password={password} \
+                                  host={host} port={port}")
+        connection_state = True
+
+        print(f'connected to {dbname}...')
+        cur = conn.cursor()
+        conn.set_session(autocommit=True)
+        print('session set to autocommit')
+        yield (cur, conn)
+
+    except psycopg2.Error as e:
+        raise
+
+    finally:
+        if connection_state:
+            print('closing cursors and connections')
+            cur.close()
+            conn.close()
+        else:
+            print('no connection made to the database')
